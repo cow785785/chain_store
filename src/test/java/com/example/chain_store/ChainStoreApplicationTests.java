bce8ea869cae721b509ed7d0e3c418d72f3075ca
zcbm0 <zcbm0@Tsai>
2023-05-07 20:35:45 +0800

    package

---
+++ src/test/java/com/example/chain_store/ChainStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chain_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
