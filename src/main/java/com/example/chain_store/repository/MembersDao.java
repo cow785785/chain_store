76294bc87cd1e5e27385e301116bd1abcc869ba2
zcbm0 <zcbm0@Tsai>
2023-05-09 15:44:14 +0800

    branch

--- src/main/java/com/example/chain_store/repository/MembersDao.java
+++ src/main/java/com/example/chain_store/repository/MembersDao.java
@@ -1,6 +1,9 @@
 package com.example.chain_store.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MembersDao {
+import com.example.chain_store.entity.Members;
+
+public interface MembersDao extends JpaRepository<Members, String> {
 	
 }
