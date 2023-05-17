087be5c8aee5f6fc01fd69c86862d591b194bdd3
Shirolin <jerrys40506@gmail.com>
2023-05-17 17:43:01 +0900

    fix something

--- src/main/java/com/example/chain_store/service/ifs/MembersService.java
+++ src/main/java/com/example/chain_store/service/ifs/MembersService.java
@@ -7,15 +7,17 @@ import com.example.chain_store.vo.MemberRequest;
 import com.example.chain_store.vo.MembersResponse;
 
 public interface MembersService {
-	public MembersResponse addMember(MemberRequest memberRequest);//·s¼W·|­û
+	public MembersResponse addMember(MemberRequest memberRequest);//ï¿½sï¿½Wï¿½|ï¿½ï¿½
 	
-	public List<Members> readMember();//¬d¸ß·|­û
+	public List<Members> readMember();//ï¿½dï¿½ß·|ï¿½ï¿½
 	
-	public MembersResponse readMember(MemberRequest memberRequest);//¬d¸ß·|­û
+	public MembersResponse readMember(MemberRequest memberRequest);//ï¿½dï¿½ß·|ï¿½ï¿½
 	
-	public MembersResponse selectMember(MemberRequest memberRequest);//¥Î±b¸¹©ÎID¬d¸ß·|­û
+	public MembersResponse selectMember(MemberRequest memberRequest);//ï¿½Î±bï¿½ï¿½ï¿½ï¿½IDï¿½dï¿½ß·|ï¿½ï¿½
 	
-	public MembersResponse updateMember(MemberRequest memberRequest);//§ó·s·|­û
+	public MembersResponse updateMember(MemberRequest memberRequest);//ï¿½ï¿½sï¿½|ï¿½ï¿½
 	
-	public MembersResponse deleteMember(MemberRequest memberRequest);//§R°£·|­û
+	public MembersResponse deleteMember(MemberRequest memberRequest);//ï¿½Rï¿½ï¿½ï¿½|ï¿½ï¿½
+	
+	public MembersResponse loginMember(MemberRequest memberRequest);
 }
